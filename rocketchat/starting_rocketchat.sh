6ef7f76df679c26e445989e34c2cf4b31b5bd43b
whiscard <jeremyogembo@gmail.com>
2018-02-15 18:02:24 +0300

    Fixed redirect of stout and sterr to log file

--- rocketchat/starting_rocketchat.sh
+++ rocketchat/starting_rocketchat.sh
@@ -3,7 +3,6 @@
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/opt/sonar/runner/bin:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin
 LOGFILE="/var/log/rocketchat_restart_script/startup.log"
 STANDARDLOGFILE="/var/log/rocketchat_restart_script/starting_services.log"
-ERRORLOGFILE="/var/log/rocketchat_restart_script/error_starting_services.log"
 docker_compose_rocketchat=`which docker-compose`
 docker_compose_hubot=`which docker-compose`
 WDIR="/opt/rocketchat"
@@ -17,13 +16,13 @@ if [[ $(docker inspect -f {{.State.Status}} rocketchat_rocketchat_1) == "restart
     cd $WDIR
 
     #start hubot
-    $docker_compose_rocketchat up -d rocketchat >> $STANDARDLOGFILE 2>> $ERRORLOGFILE
+    $docker_compose_rocketchat up -d rocketchat &> $STANDARDLOGFILE
 
     sleep 5
 
     #start rocketchat
     cd $WDIR
-    $docker_compose_hubot up -d hubot >> $STANDARDLOGFILE 2>> $ERRORLOGFILE
+    $docker_compose_hubot up -d hubot &> $STANDARDLOGFILE
 else
     echo The container is running $(date) 2> /dev/null
 fi
