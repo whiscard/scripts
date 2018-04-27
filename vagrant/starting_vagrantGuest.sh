0771974de993205c3385c4b80647b8b9a9239d95
whiscard <jeremyogembo@gmail.com>
2018-04-27 08:35:36 +0300

    Added the aborted status

--- vagrant/starting_vagrantGuest.sh
+++ vagrant/starting_vagrantGuest.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/opt/sonar/runner/bin:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin
-LOGFILE="/var/log/startupscripts/vagrant_startup.log"
-STANDARDLOGFILE="/var/log/startupscripts/vagrant_startingGuest.log"
+LOGFILE="/var/log/startupscripts/idempiere/vagrant_startup.log"
+STANDARDLOGFILE="/var/log/startupscripts/idempiere/vagrant_startingGuest.log"
 vagrant_idemp=`which vagrant`
 WDIR="/opt/vms/vagrant/idempiere/erp_academy"
 
 cd $WDIR
 
 #check if vagrant idempiere VM  is running 
-if [[ $($vagrant_idemp status | grep poweroff) == "default                   poweroff (virtualbox)" ]]; then
+if [[ $($vagrant_idemp status | grep 'poweroff (virtualbox)') == "default                   poweroff (virtualbox)" ]] || [[ $($vagrant_idemp status | grep 'aborted (virtualbox)') == "default                   aborted (virtualbox)" ]];then
     echo Vagrant Idempiere instance is not running $(date) >> $LOGFILE
         
-    #Go to the rocketchat directory
+    #Go to the vagrant idempiere directory
     cd $WDIR
 
     #start vagrant
